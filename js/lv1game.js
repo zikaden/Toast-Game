d5da1f91d16c8ae21b906b72dbe745e21bc90b66
Jule <bingthemachine@gmail.com>
2022-06-15 11:46:14 +0200

    ifPressed funciton Problem

--- js/lv1game.js
+++ js/lv1game.js
@@ -5,6 +5,10 @@ let lv1stawjam;
 let lv1curserjam;
 let lv1hoverjam;
 
+//VARIABLES click events
+let jamspace = false;
+let counter = 0;
+
 //PRELOADING all assetts
 function preload() {
     lv1bground = loadImage('/assets/lv1_background.png');
@@ -15,7 +19,9 @@ function preload() {
 }
 //SETUP
 function setup() {
-    createCanvas(1200, 800);
+    let canvas = createCanvas(1200, 800);
+    canvas.parent("canvas")
+    background(255)
 }
 
 //RESIZING of images to fit the canvas
@@ -24,6 +30,7 @@ function lv1resizeImage() {
     lv1toast.resize(480, 512);
     lv1stawjam.resize(354, 306);
     lv1hoverjam.resize(92, 84);
+    lv1curserjam.resize(92, 84);
 }
 
 //MOUSE HOVER over jam changes curser to jam curser
@@ -32,6 +39,15 @@ function lv1MouseHoverJam() {
         cursor(image(lv1hoverjam, mouseX - 35, mouseY - 30));
     }
 }
+//Mouse click on jam to t get jam and click on jam to get normal curser
+function mouseClicked() {
+    if ((mouseX > 806 && mouseX < 1100) && (mouseY > 448 && mouseY < 730)) {
+        jamspace = true;
+    } else {
+        jamspace = false;
+    }
+}
+
 
 
 //RENDERING my game
@@ -44,4 +60,15 @@ function draw() {
     image(lv1stawjam, 806, 448);
     //initializing mousehover effect
     lv1MouseHoverJam();
+    //initializing take jam when clicking on jamglass
+    if (jamspace) {
+        cursor(image(lv1curserjam, mouseX - 35, mouseY - 30));
+    }
+    //initializing jamming when having jam
+    if (mouseIsPressed) {
+        if (jamspace) {
+            ellipse(mouseX, mouseY, 10, 10);
+            console.log('jamjamjamajam')
+        }
+    }
 }
